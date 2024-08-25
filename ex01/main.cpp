46e617040481d7e6600b9055cfbef9d23d7e13bb
Larz <larz@Larz.myguest.virtualbox.org>
2024-08-25 17:40:47 +0200

    finished ex01

--- ex01/main.cpp
+++ ex01/main.cpp
@@ -2,10 +2,20 @@
 
 int main( void )
 {
-	Fixed a( 0.f );
+	Fixed a;
+	Fixed const b( 10 );
+	Fixed const c( 42.42f );
+	Fixed const d( b );
 
-	std::cout << a << " then " << ++a << std::endl;
-	std::cout << a++ << " then " << a << std::endl;
+	a = Fixed( 1234.4321f );
 	
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
 	return 0;
 }
